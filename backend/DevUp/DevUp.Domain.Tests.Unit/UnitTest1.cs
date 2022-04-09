5ee4717cc4b3dd052468ee5bcfa66258941a1166
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-04-09 19:46:44 +0200

    Add unit test project for DevUp.Domain

---
+++ backend/DevUp/DevUp.Domain.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace DevUp.Domain.Tests.Unit
+{
+    public class PurpusefullyFailingTests
+    {
+        [Test]
+        public void Fail()
+        {
+            Assert.Fail();
+        }
+    }
+}
