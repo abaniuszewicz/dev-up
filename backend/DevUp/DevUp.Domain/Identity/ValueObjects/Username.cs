e92a48614a0162415fa1d01251ee128145dae42b
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-05-19 17:17:18 +0200

    add username value object

---
+++ backend/DevUp/DevUp.Domain/Identity/ValueObjects/Username.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DevUp.Domain.Seedwork;
+using DevUp.Domain.Seedwork.Exceptions;
+
+namespace DevUp.Domain.Identity.ValueObjects
+{
+    public class Username : ValueObject
+    {
+        public string Value { get; set; }
+
+        public Username(string username)
+        {
+            Validate(username);
+            Value = username;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+
+        private static void Validate(string username)
+        {
+            if (username is null)
+                throw new ValidationException("Username cannot be null");
+
+            var errors = new List<string>();
+            if (username.Length < 6 || username.Length > 30)
+                errors.Add("Username must be 6-30 characters long.");
+            if (username.Any(c => !IsValidCharacter(c)))
+                errors.Add("Username may only contain alphanumeric characters or hyphens.");
+            if (username.StartsWith('-') || username.EndsWith('-'))
+                errors.Add("Username cannot begin or end with a hyphen.");
+        }
+
+        private static bool IsValidCharacter(char c)
+        {
+            return c switch
+            {
+                >= 'a' and <= 'z' => true,
+                '-' => true,
+                _ => false
+            };
+        }
+    }
+}
