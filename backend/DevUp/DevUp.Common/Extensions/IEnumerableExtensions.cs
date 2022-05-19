c765bda6680e4eac39b75af379894dc664cc6cd9
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-05-19 17:12:11 +0200

    add enumerable extensions

---
+++ backend/DevUp/DevUp.Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DevUp.Common.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+        {
+            foreach (var element in enumerable)
+                action(element);
+        }
+    }
+}
