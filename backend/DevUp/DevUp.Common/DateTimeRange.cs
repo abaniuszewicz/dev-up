22e6a93c5537c10bac53d6c218fa2b7559dcc78e
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-05-25 08:20:57 +0200

    use datetimerange instead of start/end

---
+++ backend/DevUp/DevUp.Common/DateTimeRange.cs
@@ -0,0 +1,22 @@
+﻿namespace DevUp.Common
+{
+    public struct DateTimeRange
+    {
+        public DateTime Start { get; init; }
+        public DateTime End { get; init; }
+
+        public DateTimeRange(DateTime start, DateTime end)
+        {
+            if (start > end)
+                throw new ArgumentException($"{nameof(start)} date can't be greater than {nameof(end)} date");
+
+            Start = start;
+            End = end;
+        }
+
+        public bool IsWithinRange(DateTime date)
+        {
+            return Start <= date && End <= date;
+        }
+    }
+}
--- backend/DevUp/DevUp.Domain/Identity/Entities/RefreshToken.cs
+++ backend/DevUp/DevUp.Domain/Identity/Entities/RefreshToken.cs
@@ -1,12 +1,17 @@
-﻿using DevUp.Domain.Identity.Entities;
+﻿using System;
+using DevUp.Common;
+using DevUp.Domain.Identity.Entities;
 using DevUp.Domain.Seedwork;
 
 namespace DevUp.Domain.Identity.ValueObjects
 {
     public class RefreshToken : Entity<RefreshTokenId>
     {
-        public bool Exists { get; }
-        public bool Invalidated { get; }
+        public string Jti { get; init; }
+        public UserId UserId { get; init; }
+        public DateTimeRange Lifespan { get; init; }
+        public bool Used { get; init; }
+        public bool Invalidated { get; init; }
 
         public RefreshToken(RefreshTokenId id) : base(id)
         {
