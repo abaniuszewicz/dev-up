c7a6fc193d6c7d839121948c44d2b45bc0c0d2ed
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-06-20 10:15:46 +0200

    add invalidate signatures to interfaces

--- backend/DevUp/DevUp.Domain/Identity/Repositories/IRefreshTokenRepository.cs
+++ backend/DevUp/DevUp.Domain/Identity/Repositories/IRefreshTokenRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using DevUp.Domain.Identity.Entities;
 using DevUp.Domain.Seedwork;
@@ -8,5 +9,7 @@ namespace DevUp.Domain.Identity.Repositories
     public interface IRefreshTokenRepository : IRepository<RefreshTokenInfo, RefreshToken>
     {
         public Task<RefreshTokenInfo> MarkAsUsedAsync(RefreshTokenInfo refreshToken, CancellationToken cancellationToken);
+        public Task<IReadOnlyList<RefreshTokenInfo>> GetDescendants(RefreshTokenInfo refreshToken, CancellationToken cancellationToken);
+        public Task<RefreshTokenInfo> Invalidate(RefreshTokenInfo refreshToken, CancellationToken cancellationToken);
     }
 }
