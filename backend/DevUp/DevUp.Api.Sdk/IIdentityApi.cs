0316dd8a81376543d368c5cdc9c3c6d8ba81ac2d
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-07-13 11:49:15 +0200

    rename DevUp.Api.Sdk to DevUp.Sdk

---
+++ backend/DevUp/DevUp.Api.Sdk/IIdentityApi.cs
@@ -0,0 +1,19 @@
+﻿using DevUp.Api.Contracts.V1.Identity.Requests;
+using DevUp.Api.Contracts.V1.Identity.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+
+namespace DevUp.Api.Sdk
+{
+    public interface IIdentityApi
+    {
+        [Post("api/v1/identity/register")]
+        public Task<ApiResponse<IdentityResponse>> Register([FromBody] RegisterUserRequest request, CancellationToken cancellationToken);
+
+        [Post("api/v1/identity/login")]
+        public Task<ApiResponse<IdentityResponse>> Login([FromBody] LoginUserRequest request, CancellationToken cancellationToken);
+
+        [Post("api/v1/identity/refresh")]
+        public Task<ApiResponse<IdentityResponse>> Refresh([FromBody] RefreshUserRequest request, CancellationToken cancellationToken);
+    }
+}
