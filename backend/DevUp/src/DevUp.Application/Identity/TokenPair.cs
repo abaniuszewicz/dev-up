08cbbad6010251d152c776f38ccab0d2383f551e
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-09-06 16:25:42 +0200

    make TokenPair { get; init; }

--- backend/DevUp/src/DevUp.Application/Identity/TokenPair.cs
+++ backend/DevUp/src/DevUp.Application/Identity/TokenPair.cs
@@ -2,13 +2,7 @@
 {
     public class TokenPair
     {
-        public string Token { get; }
-        public string RefreshToken { get; }
-
-        public TokenPair(string token, string refreshToken)
-        {
-            Token = token;
-            RefreshToken = refreshToken;
-        }
+        public string Token { get; init; }
+        public string RefreshToken { get; init; }
     }
 }
