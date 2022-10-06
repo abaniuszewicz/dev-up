31da900ea196b11f3a6777238b3fcf6cbc42c15c
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-10-06 11:23:59 +0200

    add revoke endpoint + mediatr command

---
+++ backend/DevUp/src/DevUp.Application/Identity/Commands/RevokeTokenCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace DevUp.Application.Identity.Commands
+{
+    public sealed class RevokeTokenCommand : IRequest
+    {
+        public string RefreshToken { get; init; }
+    }
+}
