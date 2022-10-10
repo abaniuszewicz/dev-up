7a39c6f8535641be1b901cb8fdf671a9fd92fc6d
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-10-10 15:36:11 +0200

    add migration that adds column for chaining refresh tokens

---
+++ backend/DevUp/src/DevUp.Infrastructure.Postgres/Migrations/AddChainColumnsToRefreshTokenTable_2022101000001.cs
@@ -0,0 +1,35 @@
+﻿using FluentMigrator;
+
+namespace DevUp.Infrastructure.Postgres.Migrations
+{
+    [Migration(2022101000001)]
+    public class AddChainColumnsToRefreshTokenTable_2022101000001 : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("refresh_tokens")
+                .AddColumn("previous").AsAnsiString().ForeignKey("refresh_tokens", "token")
+                .AddColumn("next").AsAnsiString().ForeignKey("refresh_tokens", "token");
+
+            Create.ForeignKey()
+                .FromTable("refresh_tokens").ForeignColumn("previous")
+                .ToTable("refresh_tokens").PrimaryColumn("token");
+            Create.ForeignKey()
+                .FromTable("refresh_tokens").ForeignColumn("next")
+                .ToTable("refresh_tokens").PrimaryColumn("token");
+        }
+
+        public override void Down()
+        {
+            Delete.ForeignKey()
+                .FromTable("refresh_tokens").ForeignColumn("next")
+                .ToTable("refresh_tokens").PrimaryColumn("token");
+            Delete.ForeignKey()
+                .FromTable("refresh_tokens").ForeignColumn("previous")
+                .ToTable("refresh_tokens").PrimaryColumn("token");
+
+            Delete.Column("next").FromTable("refresh_tokens");
+            Delete.Column("previous").FromTable("refresh_tokens");
+        }
+    }
+}
