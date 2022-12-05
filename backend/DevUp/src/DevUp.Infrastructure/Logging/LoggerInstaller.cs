f6b4e3cd77b358d558aa28935073266ca086025e
Adam Baniuszewicz <abaniuszewicz@gmail.com>
2022-12-05 17:59:20 +0100

    adjust elasticsearch sink options

--- backend/DevUp/src/DevUp.Infrastructure/Logging/LoggerInstaller.cs
+++ backend/DevUp/src/DevUp.Infrastructure/Logging/LoggerInstaller.cs
@@ -1,8 +1,9 @@
-﻿using System;
+﻿using System.Linq;
 using DevUp.Infrastructure.Logging.Setup;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
+using Serilog.Filters;
 using Serilog.Sinks.Elasticsearch;
 
 namespace DevUp.Infrastructure.Logging
@@ -11,11 +12,13 @@ namespace DevUp.Infrastructure.Logging
     {
         public static IServiceCollection AddLogger(this IServiceCollection services, IConfiguration configuration)
         {
-            var options = configuration.GetRequiredSection("Elasticsearch").Get<ElasticsearchOptions>();
-            var sinkOptions = new ElasticsearchSinkOptions(options.Uri)
+            var options = configuration.GetRequiredSection("Logger").Get<LoggerOptions>();
+            var sinkOptions = new ElasticsearchSinkOptions(options.ElasticsearchOptions.Uri)
             {
-                IndexFormat = $"devup-logs-{DateTime.UtcNow:yyyy-MM}",
-                AutoRegisterTemplate = true
+                IndexFormat = "devup-logs-{0:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv7,
+                TypeName = null
             };
 
             Log.Logger = new LoggerConfiguration()
@@ -24,6 +27,8 @@ namespace DevUp.Infrastructure.Logging
                 .Enrich.WithEnvironmentName()
                 .WriteTo.Console()
                 .WriteTo.Elasticsearch(sinkOptions)
+                .ReadFrom.Configuration(configuration, sectionName: "Logger:Serilog")
+                .Filter.ByExcluding(Matching.WithProperty<string>("RequestPath", rp => options.ExcludePaths.Any(ep => ep.EndsWith(rp))))
                 .CreateLogger();
 
             services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(dispose: true));
